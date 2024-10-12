310e6a60aa182d33cc41e8923f2b2b7db8ae12dd
UST1N <olegustich@mail.ru>
2024-10-12 13:41:13 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 'amahasla', 2024, False
+print(immutable_var)
+#immutable_var[1]=2020 # Кортеж не поддерживает обращение по элементам.
+#print(immutable_var)
+mutable_list = ['amahasla', 2024, False]
+mutable_list[0]='amahasler'
+mutable_list[1]=2020
+print(mutable_list)
